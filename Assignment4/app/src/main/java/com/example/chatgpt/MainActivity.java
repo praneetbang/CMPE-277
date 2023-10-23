d3c3985ad8485c8f21d4d81b9c710209a0c0f122
Praneet Bang <praneet.bang@sjsu.edu>
2023-10-22 21:45:08 -0700

    Fix api key

--- Assignment4/app/src/main/java/com/example/chatgpt/MainActivity.java
+++ Assignment4/app/src/main/java/com/example/chatgpt/MainActivity.java
@@ -110,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body=RequestBody.create(jsonBody.toString(),JSON);
         Request request=new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","")
                 .post(body)
                 .build();
 
